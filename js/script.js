1187fdc4a7c4bbc1bc0d792f0acdeabc0e2c79f1
Johnathan <jbwierzbicki@gmail.com>
2020-04-07 15:47:56 -0400

    started navbar rework (dropdown menu addition)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function navExpand() {
-    var x = document.getElementById("rwTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
